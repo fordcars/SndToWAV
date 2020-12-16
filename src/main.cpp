94b89208f89847819192c3589ac8c0c06bc00280
Carl Hewett <corteocarl@gmail.com>
2020-12-16 05:08:57 -0500

    Fix compilation missing header

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with SndToWAV. If not, see <http://www.gnu.org/licenses/>.
 
-#include "main.hpp"
-
 #include "SndToWAV.hpp"
 
 #include <cstddef> // For size_t
